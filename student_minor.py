8f250bd0c5a70f0df9ef27a6b4fc2daa229ab77a
MSA2022Minor <108094287+MSA2022Minor@users.noreply.github.com>
2022-06-28 14:41:33 -0500

    Update student_minor.py
    broken

--- student_minor.py
+++ student_minor.py
@@ -1,54 +1,59 @@
-#print the menu
-from ast import While
-import math
+print the menu
 
+def print_main_menu():
+    print("Select option from Menu\n-----------------------")
+    print("1. Login")
+    print("2. Create User")
+    return
 
-print("Select option from Menu\n-----------------------")
-print("1. Login")
-print("2. Create User")
-
-#prompt and get the option the user selected
-while True:
-    user_option = input("Would you like to (1) login or (2)create account? ")
-    #Ensure the user entered a valid option (1 or 2)
-    if user_option != "1" and user_option != "2":
-        # -if not, prompt user again
-        print("\nERROR: Enter a 1 or 2")
-        continue
-    else:
-        print("YaY! Good input")
-        break     
-
-
-if user_option == "1":
+def get_user_option():
+    print_main_menu
     while True:
-        #If user chose 1, ask for user name and password and
-        user_name = input("Please enter your user name: ")
-        user_pass = input("Please enter your password: ")
-        # - validate username and password combination in the users.txt file
-        #open the users files
-        user_file = open("users.txt", "r")
-        user_found = False
-
-        #read the lines from the file
-        for line in user_file:
-            credentials = line.split(", ")
-             #compare username and password for a match
-            if user_name == credentials[0] and user_pass == credentials[1].rstrip():
-                user_found = True
-                break
-
-        if user_found:
-            # - if valid then move on to prompt for student data
-            print(f"User {user_name} successfully logged in!\n")
-            break
+        user_option = input("Would you like to (1) login or (2)create account? ")
+        #Ensure the user entered a valid option (1 or 2)
+        if user_option != "1" and user_option != "2":
+            # -if not, prompt user again
+            print("\nERROR: Enter a 1 or 2")
+            continue
         else:
-            # - if not valid combination reprompt the user. 
-            print(f"User {user_name} not found!\n")
+            print("YaY! Good input")
+            break     
+    return user_option
+#prompt and get the option the user selected
+def main():
+    print_main_menu()
+    user_option = get_user_option()
+
+
+    if user_option == "1":
+        while True:
+            #If user chose 1, ask for user name and password
+            user_name = input("Please enter your user name: ")
+            user_pass = input("Please enter your password: ")
+            # - validate username and password combination in the users.txt file
+            #open the users files
+            user_file = open("users.txt", "r")
+            user_found = False
+
+            #read the lines from the file
+            for line in user_file:
+                credentials = line.split(", ")
+                #compare username and password for a match
+                if user_name == credentials[0] and user_pass == credentials[1].rstrip():
+                    user_found = True
+                    break
+
+            if user_found:
+                # - if valid then move on to prompt for student data
+                print(f"User {user_name} successfully logged in!\n")
+                break
+            else:
+                # - if not valid combination reprompt the user. 
+                print(f"User {user_name} not found!\n")
+            
         
-       
-        
-        
+            
+            
 
 #If user chose 2, ask for user name and password and
 elif user_option == "2":
@@ -102,9 +107,27 @@ for index in range(len(student_names)):
     print(f"{student_names[index]} : {int(student_scores[index])}% : {student_letter_grades[index]}")
 
 #Calculate and print class average
+average_list = []
+
 total = 0
 for index in range(len(student_scores)):
     total = total + student_scores[index]
 average = total / len(student_scores)
 
-print(f"Average: {int(round(average, 2))}%")
+if average >= 90:
+        average_list.append("A")
+elif average >= 80:
+        average_list.append("B")
+elif average >= 70:
+        average_list.append("C")
+elif average >= 60:
+        average_list.append("D")
+else:
+        average_list.append("F")
+        
+print(f"Average: {int(round(average, 2))}% {average_list}")
+
+
+
+
+    
